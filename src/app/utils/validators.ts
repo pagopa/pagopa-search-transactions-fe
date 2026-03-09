3df6f346f33d4a0e2db461ab881a98d91a4e0547
FrancescoDiTonnoJakala <francesco.ditonno.ext@jakala.com>
2026-03-09 10:28:30 +0100

    Update src/app/utils/validators.ts
    Co-authored-by: ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -4,7 +4,7 @@ export const normalize = (v: string) => v.trim().toUpperCase();
 
 export const matchFiscalCode = (v: string) => /^[A-Z0-9]{11,16}$/i.test(v.trim());
 
-export const isProbablyNav = (v: string) => /^[A-Z0-9]{6,35}$/i.test(v.trim());
+export const matchNav = (v: string) => /^[A-Z0-9]{6,35}$/i.test(v.trim());
 
 export function validateSearchInput(input: {
   enteFiscalCode: string;
