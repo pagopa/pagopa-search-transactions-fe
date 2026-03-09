07a777b6ab3296e526e136ab9aa859f3976c4740
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-03-09 17:40:17 +0100

    enable standalone copy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
   output: 'standalone',
   assetPrefix: '/pagopa-search-transactions-fe',
 };
