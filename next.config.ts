4130057a28ae8fe776fa2e2c88a72f97dd837e4c
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-03-09 17:33:26 +0100

    set next configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone',
+  assetPrefix: '/pagopa-search-transactions-fe',
 };
 
 export default nextConfig;
